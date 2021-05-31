d46629c31832a5e56cb05dc7584a1a30ef202971
oliv264p <61137942+oliv264p@users.noreply.github.com>
2021-05-31 11:36:55 +0200

    filtrering commit

--- loudtheme/filtrering.js
+++ loudtheme/filtrering.js
@@ -1,51 +1,28 @@
-        function opretKnapper() {
 
+
+
+        function opretKnapper() {
             console.log("opretKnapper");
 
-
             categories.forEach(cat => {
-
-                document.querySelector("#filtrering").innerHTML += < button class = "filter"
-
-                data - produkt = "${cat.id}" > $ {
-
-                    cat.name
-
-                } < /button>
-
+                document.querySelector("#filtrering").innerHTML += `<button class="filter" data-podcast="${cat.id}">${cat.name}</button>`
             })
-
 
             addEventListenerToButtons();
-
         }
 
-
-
         function addEventListenerToButtons() {
-
             document.querySelectorAll("#filtrering button").forEach(elm => {
-
                 elm.addEventListener("click", filtrering);
-
             })
-
         };
 
-
-
         function filtrering() {
-
-            filterProdukt = this.dataset.produkt;
-
+            filterPodcast = this.dataset.podcast;
             document.querySelector(".valgt").classList.remove("valgt");
-
             this.classList.add("valgt");
 
+            console.log(filterPodcast);
 
-            console.log(filterProdukt);
-
-
-            visProdukter();
-
+            visPodcasts();
         }
--- loudtheme/page-produkter.php
+++ loudtheme/page-produkter.php
@@ -59,6 +59,32 @@ get_header();
             opretKnapper();
         }
 
+        function opretKnapper() {
+            console.log("opretKnapper");
+
+            categories.forEach(cat => {
+                document.querySelector("#filtrering").innerHTML += `<button class="filter" data-podcast="${cat.id}">${cat.name}</button>`
+            })
+
+            addEventListenerToButtons();
+        }
+
+        function addEventListenerToButtons() {
+            document.querySelectorAll("#filtrering button").forEach(elm => {
+                elm.addEventListener("click", filtrering);
+            })
+        };
+
+        function filtrering() {
+            filterPodcast = this.dataset.podcast;
+            document.querySelector(".valgt").classList.remove("valgt");
+            this.classList.add("valgt");
+
+            console.log(filterPodcast);
+
+            visPodcasts();
+        }
+
 
 
         function visProdukter() {
