cfbd5ccbc0ce23317dc36e1009d8083f5ef175d4
oliv264p <61137942+oliv264p@users.noreply.github.com>
2021-05-31 10:46:03 +0200

    hul igennem rest

--- loudtheme/page-produkter.php
+++ loudtheme/page-produkter.php
@@ -59,34 +59,7 @@ get_header();
             opretKnapper();
         }
 
-        function opretKnapper() {
-            console.log("opretKnapper");
 
-            categories.forEach(cat => {
-                document.querySelector("#filtrering").innerHTML += < button class = "filter"
-                data - produkt = "${cat.id}" > $ {
-                    cat.name
-                } < /button>
-            })
-
-            addEventListenerToButtons();
-        }
-
-        function addEventListenerToButtons() {
-            document.querySelectorAll("#filtrering button").forEach(elm => {
-                elm.addEventListener("click", filtrering);
-            })
-        };
-
-        function filtrering() {
-            filterProdukt = this.dataset.produkt;
-            document.querySelector(".valgt").classList.remove("valgt");
-            this.classList.add("valgt");
-
-            console.log(filterProdukt);
-
-            visProdukter();
-        }
 
         function visProdukter() {
             console.log("visProdukter");
