cc9ef212ca946c645c4dd4249e20133a0b437a28
oliv264p <61137942+oliv264p@users.noreply.github.com>
2021-06-01 10:15:22 +0200

    size

--- loudtheme/page-produkter.php
+++ loudtheme/page-produkter.php
@@ -24,7 +24,7 @@ get_header();
 <!--template-->
 <template>
     <article class="produkt_article">
-         <img src="" alt="" width="400" height="500">
+         <img src="" alt="" width="300" height="300">
         <div>
             <h2></h2>
             <h3></h3>
