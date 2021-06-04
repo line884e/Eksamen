464ebeda1cf687f54b26d397be2c1da3f8b94d9a
oliv264p <61137942+oliv264p@users.noreply.github.com>
2021-06-04 14:11:05 +0200

    push

---
+++ loudtheme/backup/single-produkter.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since Twenty Nineteen 1.0
+ */
+
+get_header();
+?>
+
+<div class="pil">
+    <a href="http://up2create.dk/kea/2-semester/eksamen/wordpress/produkt/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/pil.png" alt="pil" class="pil"></a>
+</div>
+
+<!--template-->
+<section id="primary" class="content-area">
+    <main id="main" class="site-main">
+        <h1></h1>
+        <article class="martin">
+            <div class="wrapper">
+                <div class="col1">
+                    <h1></h1>
+                    <p class="beskrivelse_produkt"></p>
+                    <p class="pris"></p>
+                </div>
+                <div class="col2">
+                    <img src="" alt="">
+                </div>
+            </div>
+        </article>
+
+
+    </main>
+    </section>
+
+    <!--script-->
+    <script>
+        let produkt;
+        let aktuelprodukt = <?php echo get_the_ID() ?>;
+        var elmnt = document.getElementById("content");
+
+        const dbUrl = "http://up2create.dk/kea/2-semester/eksamen/wordpress/wp-json/wp/v2/produkter/" + aktuelprodukt;
+
+
+        async function getJson() {
+            const data = await fetch(dbUrl);
+            produkt = await data.json();
+            visProdukter();
+        }
+
+        function visProdukter() {
+            console.log("visProdukter");
+            document.querySelector("h1").innerHTML = produkt.title.rendered;
+            document.querySelector(".martin img").src = produkt.billede.guid;
+            document.querySelector(".beskrivelse_produkt").innerHTML = produkt.beskrivelse_produkt;
+            document.querySelector(".pris").innerHTML = "<b>Pris: </b>" + produkt.pris;
+        }
+
+        getJson();
+
+        //Get the button:
+        mybutton = document.getElementById("myBtn");
+
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function() {
+            scrollFunction()
+        };
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                mybutton.style.display = "block";
+            } else {
+                mybutton.style.display = "none";
+            }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document
+        function topFunction() {
+            document.body.scrollTop = 0; // For Safari
+            document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        }
+
+    </script>
+
+
+
+<?php
+
+get_footer();
--- loudtheme/single-produkter.php
+++ loudtheme/single-produkter.php
@@ -11,7 +11,7 @@
 
 get_header();
 ?>
- <?php the_content(); ?>
+
 <div class="pil">
     <a href="http://up2create.dk/kea/2-semester/eksamen/wordpress/produkt/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/pil.png" alt="pil" class="pil"></a>
 </div>
@@ -23,68 +23,73 @@ get_header();
         <article class="martin">
             <div class="wrapper">
                 <div class="col1">
+                    <img class="single_billede" src="" alt="">
+                </div>
+                <div class="col2">
                     <h1></h1>
                     <p class="beskrivelse_produkt"></p>
                     <p class="pris"></p>
-                </div>
-                <div class="col2">
-                    <img src="" alt="">
+                    <p class="single_p"></p>
+                    <button class="laeg_i_kurv">LÆG I KURV</button>
                 </div>
             </div>
         </article>
 
 
     </main>
-    </section>
+</section>
 
-    <!--script-->
-    <script>
-        let produkt;
-        let aktuelprodukt = <?php echo get_the_ID() ?>;
-        var elmnt = document.getElementById("content");
+<!--script-->
+<script>
+    let produkt;
+    let aktuelprodukt = <?php echo get_the_ID() ?>;
+    var elmnt = document.getElementById("content");
 
-        const dbUrl = "http://up2create.dk/kea/2-semester/eksamen/wordpress/wp-json/wp/v2/produkter/" + aktuelprodukt;
+    const dbUrl = "http://up2create.dk/kea/2-semester/eksamen/wordpress/wp-json/wp/v2/produkter/" + aktuelprodukt;
 
 
-        async function getJson() {
-            const data = await fetch(dbUrl);
-            produkt = await data.json();
-            visProdukter();
+    async function getJson() {
+        const data = await fetch(dbUrl);
+        produkt = await data.json();
+        visProdukter();
+    }
+
+    function visProdukter() {
+        console.log("visProdukter");
+        document.querySelector("h1").innerHTML = produkt.title.rendered;
+        document.querySelector(".single_billede").src = produkt.billede.guid;
+        document.querySelector(".beskrivelse_produkt").innerHTML = produkt.beskrivelse_produkt;
+        document.querySelector(".single_p").innerHTML = produkt.single;
+
+
+        document.querySelector(".pris").innerHTML = "<b>Pris: </b>" + produkt.pris;
+    }
+
+    getJson();
+
+    //Get the button:
+    mybutton = document.getElementById("myBtn");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {
+        scrollFunction()
+    };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
         }
+    }
 
-        function visProdukter() {
-            console.log("visProdukter");
-            document.querySelector("h1").innerHTML = produkt.title.rendered;
-            document.querySelector(".martin img").src = produkt.billede.guid;
-            document.querySelector(".beskrivelse_produkt").innerHTML = produkt.beskrivelse_produkt;
-            document.querySelector(".pris").innerHTML = "<b>Pris: </b>" + produkt.pris;
-        }
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
 
-        getJson();
-
-        //Get the button:
-        mybutton = document.getElementById("myBtn");
-
-        // When the user scrolls down 20px from the top of the document, show the button
-        window.onscroll = function() {
-            scrollFunction()
-        };
-
-        function scrollFunction() {
-            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-                mybutton.style.display = "block";
-            } else {
-                mybutton.style.display = "none";
-            }
-        }
-
-        // When the user clicks on the button, scroll to the top of the document
-        function topFunction() {
-            document.body.scrollTop = 0; // For Safari
-            document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-        }
-
-    </script>
+</script>
 
 
 
